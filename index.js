4d4e33d25d5a632fc24f2976ca19bb357d543718
aheitzen <alexaheitzenrader@yahoo.com>
2016-02-19 14:46:16 -0800

    installed all of my dependencies

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var request= require('request');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+	res.send('I work');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var port = 3000;
+app.listen(port, function() {
+  console.log("You're listening to the smooth sounds of port " + port);
+});

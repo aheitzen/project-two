be64edcc7b7ee6a91b75e9ff29e9467e871f564f
aheitzen <alexaheitzenrader@yahoo.com>
2016-02-20 16:55:24 -0800

    added external api picture generation

--- index.js
+++ index.js
@@ -16,19 +16,37 @@ app.get('/', function(req, res) {
 
 
 app.get('/type', function(req, res) {
-	request('https://api.pinterest.com/v1/boards/dalepartridge/typography-design/pins/?access_token=' + process.env.PINTEREST_TOKEN + '&fields=id,link,image', function(err, response, body) {
+
+
+	
+	request('https://api.pinterest.com/v1/boards/dalepartridge/typography-design/pins/?access_token=' + process.env.PINTEREST_TOKEN + '&fields=id,link,image,note', function(err, response, body) {
 		var parsedBody = JSON.parse(body); //turns it into a javascript object instead of a string
-		if(!err && response.statusCode === 200 && parsedBody.data) { //checking to see if data exists on parsedBody
-			res.render('showpage.ejs', {data: parsedBody.data});
+
+		request(parsedBody.page.next, function(err, response, body2) {
+			parsedBody = parsedBody.data.concat(parsedBody.page.next);
+			
+			if(!err && response.statusCode === 200 && parsedBody.data) { //checking to see if data exists on parsedBody
+		
+			var filteredPins = parsedBody.data.filter(function filterByNote(item) {
+				if(item.note.includes(req.query.q)){
+					return true;
+				} else {
+					return false;
+				}
+	
+			});
+			res.render('showpage.ejs', {data: filteredPins});
 		} else {
 			res.send(err); //later you can render
 		}
+		});
+
 	});
+		
 });
 
-app.post('/type', function(req, res) {
-	
-})
+
+
 
 
 
